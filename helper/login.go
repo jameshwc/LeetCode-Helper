c94657da5e0c3f18550437ae6ad619f7858ec414
jameshwc <b07902001@csie.ntu.edu.tw>
2020-01-12 10:51:05 +0800

    commit for another approach to use session_id to login instead

--- helper/login.go
+++ helper/login.go
@@ -3,14 +3,18 @@ package helper
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 
 	"github.com/mozillazg/request"
 )
 
-type myRequest request.Request
-type myRequestPtr *request.Request
+const (
+	loginURL  = "https://leetcode.com/accounts/login"
+	userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+)
+
 type leetCodeUser struct {
 	config userConfig
 }
@@ -27,20 +31,56 @@ func (u *userConfig) get() {
 func (u *leetCodeUser) init() {
 	u.config.get()
 }
-func (u *leetCodeUser) login() myRequestPtr {
+func (u *leetCodeUser) login() error {
 	req := request.NewRequest(new(http.Client))
+	csrfToken, cfdUID := getCSRFToken(req)
+	cookies := "__cfduid=" + cfdUID + 
+				";csrftoken=" + csrfToken 
+				+ ";"
+	fmt.Println(cookies)
 	req.Headers = map[string]string{
-		"Accept-Encoding": "",
-		"Referer":         "https://leetcode.com/",
+		"User-Agent": userAgent,
+		"Connection": "keep-alive",
+		"Referer":    "https://leetcode.com/accounts/login/",
+		"origin":     "https://leetcode.com",
+		// "Cookie":          cookies,
 	}
-	return myRequestPtr(req)
+	req.Data = map[string]string{
+		"csrfmiddlewaretoken": csrfToken,
+		"login":               u.config.username,
+		"password":            u.config.password,
+		"next":                "problems",
+		// "__cfduid":            cfdUID,
+	}
+	// fmt.Printf("%s %s", u.config.username, u.config.password)
+	response, err := req.Post(loginURL)
+	defer response.Body.Close()
+	fmt.Println(response.Status)
+	return err
 }
-func (r *myRequest) getCSRFToken() string {
-	return ""
+func getCSRFToken(r *request.Request) (string, string) {
+	response, err := r.Get(loginURL)
+	if err != nil {
+		log.Panicf("Cannot get to %s: %s", response, err)
+	}
+	cookies := response.Cookies()
+	var csrfToken, cfdUID string
+	for _, cookie := range cookies {
+		switch cookie.Name {
+		case "csrftoken":
+			csrfToken = cookie.Value
+		case "__cfduid":
+			cfdUID = cookie.Value
+		}
+	}
+	if csrfToken != "" && cfdUID != "" {
+		return csrfToken, cfdUID
+	}
+	panic("Can not find both csrftoken and cfduid in cookies!")
 }
 
 func LoginMain() {
 	var u leetCodeUser
 	u.init()
-	fmt.Println(u)
+	u.login()
 }
